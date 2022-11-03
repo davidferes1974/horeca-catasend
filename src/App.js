1725baf9f73540c8bb11ddea44a2868fe2014e47
davidferes1974 <davidfernandez@galicloud.com>
2022-11-03 19:13:09 +0100

    photo change 1

--- src/App.js
+++ src/App.js
@@ -526,7 +526,7 @@ export default function Example() {
           <div class="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
             <article class="overflow-hidden rounded-lg shadow-lg">
               <a>
-                <img class="block h-auto w-full" src="horeca2.jpg" />
+                <img class="block h-auto w-full" src="horeca2.webp" />
               </a>
 
               <header class="flex items-center justify-between leading-tight p-2 md:p-4">
