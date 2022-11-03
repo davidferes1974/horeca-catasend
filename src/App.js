c97e1fd53410b70d24d362df7153981924280965
davidferes1974 <davidfernandez@galicloud.com>
2022-11-03 19:22:51 +0100

    photo changes

--- src/App.js
+++ src/App.js
@@ -543,7 +543,7 @@ export default function Example() {
           <div class="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
             <article class="overflow-hidden rounded-lg shadow-lg">
               <a>
-                <img class="block h-auto w-full" src="horeca3.jpg" />
+                <img class="block h-auto w-full" src="horeca3.webp" />
               </a>
 
               <header class="flex items-center justify-between leading-tight p-2 md:p-4">
@@ -560,7 +560,7 @@ export default function Example() {
           <div class="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
             <article class="overflow-hidden rounded-lg shadow-lg">
               <a>
-                <img class="block h-auto w-full" src="horeca1.jpg" />
+                <img class="block h-auto w-full" src="horeca1.webp" />
               </a>
 
               <header class="flex items-center justify-between leading-tight p-2 md:p-4">
